08f391567eb3dd801e30853c7e8a1b332724df75
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-21 03:10:26 +0100

    Ahora con if y else anidados

--- example.py
+++ example.py
@@ -2,3 +2,11 @@ a = 20
 b =100
 resultado = b - a
 print 'Resultado %d' % resultado
+
+if resultado > 0:
+	print 'El resultado es mayor a 0'
+	if resultado > 100:
+		print "Mayor a 100"
+	else:
+		print "Naaa es menor que 100"
+
